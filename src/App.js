36efd59a22edfa1341e81cc6b1ed1e7bdb2b1a98
delimmaemma <calvinhtml@gmail.com>
2023-07-03 15:38:55 -0500

    Deleted homescreen buttons and replaced with just characters.

--- src/App.js
+++ src/App.js
@@ -14,24 +14,11 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [selected, setSelected] = useState(0)
 
   return (
     <div className="App">
       <Header />
-      <button className='button container star-wars-look' onClick={() => selected === 1 ? setSelected(0) : setSelected(1)}><span>Theme</span></button>
-        {(selected === 1) && <div><Theme /></div>}
-      <button className="button container star-wars-look" onClick={() => selected === 2 ? setSelected(0) : setSelected(2)}><span>Characters</span></button>
-        {(selected === 2) && <div><Character /></div>}
-      <button className='button container star-wars-look' onClick={() => selected === 3 ? setSelected(0) : setSelected(3)}><span>Films</span></button>
-        {(selected === 3) && <div><Film /></div>}
-        {/* <div style={{display:display}}>
-          <Film />
-        </div> */}
-      {/* <Character
-        characters={getFiltered()}/> */}
-      {/* <Film
-        film={getFiltered()}/> */}
+      <Character />
     </div>
   );
 }
