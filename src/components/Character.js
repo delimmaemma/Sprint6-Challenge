241aa815cd57997415887400647290d520954aea
delimmaemma <calvinhtml@gmail.com>
2023-07-03 15:49:25 -0500

    Added styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,7 @@
 import React, {useState} from 'react'
 import {data} from '../mocks/handlers'
 import Info from './Info'
+import styled from 'styled-components'
 
 import '../css/StarWarsButtons.css'
 import '../css/index.css'
@@ -14,12 +15,45 @@ export default function Character() {
         setCurrentCharacterId(null)
     }
 
+    const SecondaryButton = styled.div`
+        display: inline-block;
+        border-color: black;
+        border-radius: 4px;
+        background: none;
+        font-family: 'Star Wars', sans-serif;
+        text-align: center;
+        font-size: 28px;
+        padding: 20px;
+        width: 60%;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+        border: 2px solid rgb(210, 210, 210);
+        box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+        border-radius: 8px;
+        margin: 16px;
+        padding: 16px 8px 12px 16px;
+        &:hover {
+            box-shadow: 2px 3px 8px 0px rgb(128, 127, 127);
+        }
+        color: black;
+        text-shadow: 
+            -1px -1px 0 #FFE81F,
+            1px -1px 0 #FFE81F,
+            -1px 1px 0 #FFE81F,
+            1px 1px 0 #FFE81F,  
+            -2px 0 0 #FFE81F,
+            2px 0 0 #FFE81F,
+            0 2px 0 #FFE81F,
+            0 -2px 0 #FFE81F;
+    `
+
     return (
        <div>            
             {name.map((person) => {
                 return (
                 <div>
-                    <button className='secondary-button container star-wars-look' onClick={() => setCurrentCharacterId(person.id)} key={person.id}><span>{person.name}</span></button>
+                    <SecondaryButton className= 'secondary-button' onClick={() => setCurrentCharacterId(person.id)} key={person.id}><span>{person.name}</span></SecondaryButton>
                     {currentCharacterId && <Info characterId={currentCharacterId} person={person} close = {closeDetails} />}
                 </div>
 )})}
